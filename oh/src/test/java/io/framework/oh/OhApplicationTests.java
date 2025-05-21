d237cb28170931fce526ad2c0305ebe9e4a94aaa
hyun <dhtmdgussksm@naver.com>
2025-05-21 21:39:08 +0900

    적용

---
+++ oh/src/test/java/io/framework/oh/OhApplicationTests.java
@@ -0,0 +1,13 @@
+package io.framework.oh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
